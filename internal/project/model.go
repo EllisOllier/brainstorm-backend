c255874188b439835cffc8bdd950cbe0541aecbb
EllisOllier <ellisollier1@gmail.com>
2026-02-07 14:01:48 +0000

    Add structs and constructors for projects

--- internal/project/model.go
+++ internal/project/model.go
@@ -1,1 +1,16 @@
 package project
+
+type Project struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type ProjectService struct {
+	projectRepository *ProjectRepository
+}
+
+func NewProjectService(givenProjectRepository *ProjectRepository) *ProjectService {
+	return &ProjectService{
+		projectRepository: givenProjectRepository,
+	}
+}
