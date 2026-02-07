cb51dad0907d9e55fee400628980680255e1f9b3
EllisOllier <ellisollier1@gmail.com>
2026-02-07 17:56:15 +0000

    Add GetPageById and GetPages handler

--- internal/page/handler.go
+++ internal/page/handler.go
@@ -1,1 +1,62 @@
 package page
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/EllisOllier/brainstorm-backend/internal/middleware"
+)
+
+func (s *PageService) GetPageById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	rawId := r.Context().Value(middleware.UserIdKey)
+	userId, ok := rawId.(int)
+	if !ok {
+		http.Error(w, "Could not find user ID", http.StatusUnauthorized)
+		return
+	}
+
+	enc := json.NewEncoder(w)
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
+
+	row, err := s.pageRepository.GetPageById(id, userId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.Error(w, "Not Found: 404", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Error getting users project", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	enc.Encode(row)
+}
+
+func (s *PageService) GetPages(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	rawId := r.Context().Value(middleware.UserIdKey)
+	userId, ok := rawId.(int)
+	if !ok {
+		http.Error(w, "Could not find user ID", http.StatusUnauthorized)
+		return
+	}
+
+	enc := json.NewEncoder(w)
+
+	rows, err := s.pageRepository.GetPages(userId)
+	if err != nil {
+		http.Error(w, "Error getting users projects", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	enc.Encode(rows)
+}
