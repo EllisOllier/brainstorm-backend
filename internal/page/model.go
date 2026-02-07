c54422f47b4a1f872464a7d20dbe8be8a234064a
EllisOllier <ellisollier1@gmail.com>
2026-02-07 17:54:23 +0000

    Add stuct and constructor for page

--- internal/page/model.go
+++ internal/page/model.go
@@ -1,1 +1,17 @@
 package page
+
+type Page struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"Content"`
+}
+
+type PageService struct {
+	pageRepository *PageRepository
+}
+
+func NewPageService(givenPageRepository *PageRepository) *PageService {
+	return &PageService{
+		pageRepository: givenPageRepository,
+	}
+}
