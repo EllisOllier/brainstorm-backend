a29d6e779e68e88227f94f2aa264a357207de154
EllisOllier <ellisollier1@gmail.com>
2026-02-07 14:16:15 +0000

    Add ID to GetProjectsById

--- internal/project/repository.go
+++ internal/project/repository.go
@@ -14,7 +14,7 @@ func NewProjectRepository(givenDb *sql.DB) *ProjectRepository {
 
 func (r *ProjectRepository) GetProjectById(project_id int, user_id int) (Project, error) {
 	var temp Project
-	err := r.db.QueryRow("SELECT title, description FROM projects WHERE id = $1 AND user_id = $2", project_id, user_id).Scan(&temp.Title, &temp.Description)
+	err := r.db.QueryRow("SELECT id, title, description FROM projects WHERE id = $1 AND user_id = $2", project_id, user_id).Scan(&temp.ID, &temp.Title, &temp.Description)
 	if err != nil {
 		return temp, err
 	}
