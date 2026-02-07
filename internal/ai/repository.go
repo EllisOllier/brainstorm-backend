91a2633b59e992ed2f3ac9dba7ce722acc7dcfd6
EllisOllier <ellisollier1@gmail.com>
2026-02-07 13:36:47 +0000

    Add AddPage to store pages for project to database

--- internal/ai/repository.go
+++ internal/ai/repository.go
@@ -1,6 +1,9 @@
 package ai
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type AiRepository struct {
 	db *sql.DB
@@ -16,6 +19,17 @@ func (r *AiRepository) AddProject(project Project, userId int) (int, error) {
 	var entryId int
 	err := r.db.QueryRow("INSERT INTO projects (title, description, user_id) VALUES ($1, $2, $3) RETURNING id", project.Title, project.Description, userId).Scan(&entryId)
 	if err != nil {
+		fmt.Printf("Database error: %v\n", err)
+		return -1, err // -1 error value
+	}
+	return entryId, nil
+}
+
+func (r *AiRepository) AddPage(page Page, projectId int, userId int) (int, error) {
+	var entryId int
+	err := r.db.QueryRow("INSERT INTO pages (title, content, project_id, user_id) VALUES ($1, $2, $3, $4) RETURNING id", page.Title, page.Content, projectId, userId).Scan(&entryId)
+	if err != nil {
+		fmt.Printf("Database error: %v\n", err)
 		return -1, err // -1 error value
 	}
 	return entryId, nil
