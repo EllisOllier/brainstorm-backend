90fd053693ce5b218f7b0726881a083846551f24
EllisOllier <ellisollier1@gmail.com>
2026-02-07 14:15:42 +0000

    Add GetProjects

--- internal/project/repository.go
+++ internal/project/repository.go
@@ -21,3 +21,21 @@ func (r *ProjectRepository) GetProjectById(project_id int, user_id int) (Project
 
 	return temp, nil
 }
+
+func (r *ProjectRepository) GetProjects(userId int) ([]Project, error) {
+	projects := []Project{}
+	rows, err := r.db.Query("SELECT id, title, description FROM projects WHERE user_id=$1", userId)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var temp Project
+		rows.Scan(&temp.ID, &temp.Title, &temp.Description)
+		projects = append(projects, temp)
+	}
+
+	return projects, nil
+}
